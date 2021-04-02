faf1380348a5d119b2bc19d871f76ce1a5c9f98a
Andrew Olson <frrrkin@gmail.com>
2021-04-01 22:58:05 -0700

    added event bubbling to pass through the data-id attribute so appropriate routing can be done from the client

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,37 @@
-function thing() {
+const jqEatenParent = $("#eaten-parent");
+const jqUneatenParent = $("#uneaten-parent");
 
-}
+
 
 function main() {
     $('#abutton').click(function(ev){
         alert("clicky-clicky")
     })
+    jqEatenParent.on("click", function(ev){
+        ev.preventDefault();
+        let target = ev.target;
+        console.log("ev.target.type:", ev.target.type);
+        if(ev.target.type !== "submit")
+        {
+            return;
+        }
+        console.log("You clicked on one of my eaten children");
+        console.log("data-id: ", target.getAttribute("data-id"));
+    });
+    jqUneatenParent.on("click", function(ev){
+        ev.preventDefault();
+        let target = ev.target;
+        console.log("ev.target.type:", ev.target.type);
+        if(ev.target.type !== "submit")
+        {
+            return;
+        }
+        console.log("You clicked on one of my uneaten children");
+        console.log("data-id: ", target.getAttribute("data-id"));
+    });
 }
 
-main()
+$(function (){
+    main();
+});
+
